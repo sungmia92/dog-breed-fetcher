f8acbd01dbdadc90cffa45366fd6c1f19f575168
sungmia92 <mia.sung@mail.utoronto.ca>
2025-10-19 23:43:22 -0400

    Edited DogApiBreedFetcher again

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -6,41 +6,27 @@ import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.*;
 
-/**
- * BreedFetcher implementation that relies on the dog.ceo API.
- * Note that all failures get reported as BreedNotFoundException
- * exceptions to align with the requirements of the BreedFetcher interface.
- */
 public class DogApiBreedFetcher implements BreedFetcher {
     private final OkHttpClient client = new OkHttpClient();
 
-    /**
-     * Fetch the list of sub breeds for the given breed from the dog.ceo API.
-     *
-     * @param breed the breed to fetch sub breeds for
-     * @return list of sub breeds for the given breed
-     * @throws IOException if a network error occurs
-     * @throws BreedNotFoundException if the breed does not exist or the API returns an error
-     */
     @Override
-    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+    public List<String> getSubBreeds(String breed) throws BreedFetcher.BreedNotFoundException {
         try {
             String url = "https://dog.ceo/api/breed/" + breed + "/list";
             Request request = new Request.Builder().url(url).build();
 
             try (Response response = client.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
-                    throw new BreedNotFoundException(breed);
+                    throw new BreedFetcher.BreedNotFoundException(breed);
                 }
 
                 String responseBody = response.body().string();
                 JSONObject jsonObject = new JSONObject(responseBody);
 
                 if (!"success".equals(jsonObject.getString("status"))) {
-                    throw new BreedNotFoundException(breed);
+                    throw new BreedFetcher.BreedNotFoundException(breed);
                 }
 
                 JSONArray messageArray = jsonObject.getJSONArray("message");
@@ -51,9 +37,9 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
                 return subBreeds;
             }
-        } catch (IOException e) {
-            // Wrap IOException as BreedNotFoundException for compatibility with tests
-            throw new BreedNotFoundException(breed);
+        } catch (Exception e) {
+            // Catch all IO and JSON parsing exceptions and wrap as BreedNotFoundException
+            throw new BreedFetcher.BreedNotFoundException(breed);
         }
     }
 }
